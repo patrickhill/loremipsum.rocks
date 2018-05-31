7fecb026512725d8261bd2ba7513c54fd931b149
Patrick Hill <patrickrhill@gmail.com>
2018-05-31 14:42:44 -0400

    adds alert

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@
 // wrapper element with .copy-to-clipboard class
 // 1 child element, usually a button, responsible for activating copy and paste with .clipboard-init class
 // 1 child element with the content to be copied with class .clipboard-content
+var alertEl = document.getElementById('alert-copied');
 
 var codeCopy = document.querySelectorAll('.copy-to-clipboard'), i;
 for (i = 0; i < codeCopy.length; ++i) {
@@ -14,6 +15,20 @@ for (i = 0; i < codeCopy.length; ++i) {
     var copyContent = this.parentNode.querySelector(".clipboard-content");
     copyToClipboardCode(copyContent); 
   }, true); 
+}
+
+function triggerAnimation(el, animation) {
+  el.classList.remove(animation);
+  
+  // -> triggering reflow /* The actual magic */
+  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+  // Oops! This won't work in strict mode. Thanks Felis Phasma!
+  // element.offsetWidth = element.offsetWidth;
+  // Do this instead:
+  void el.offsetWidth;
+  
+  // -> and re-adding the class
+  el.classList.add(animation);
 }
 
 function copyToClipboardCode(copyContent) {
@@ -30,6 +45,9 @@ function copyToClipboardCode(copyContent) {
     var successful = document.execCommand('copy');  
     var msg = successful ? 'successful' : 'unsuccessful';  
     console.log('Copy text command was ' + msg);  
+    if (successful) {
+      triggerAnimation(alertEl,"go");
+    }
   } catch(err) {  
     console.log('Oops, unable to copy');  
   }  
